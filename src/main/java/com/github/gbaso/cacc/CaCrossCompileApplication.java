727a4c7dd6a6224904f73671295fa1b3a51b1b80
Giacomo Baso <gbaso@users.noreply.github.com>
2025-08-04 15:34:38 +0200

    initial commit

---
+++ src/main/java/com/github/gbaso/cacc/CaCrossCompileApplication.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.cacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaCrossCompileApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CaCrossCompileApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/gbaso/cacc/CaCrossCompileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.cacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaCrossCompileApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
