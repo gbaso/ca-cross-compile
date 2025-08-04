eb05c27b60f2bb66790e650c7e0b5aa8311d5abd
Giacomo Baso <gbaso@users.noreply.github.com>
2025-08-04 15:40:49 +0200

    add Hello World log message on startup

--- src/main/java/com/github/gbaso/cacc/CaCrossCompileApplication.java
+++ src/main/java/com/github/gbaso/cacc/CaCrossCompileApplication.java
@@ -1,13 +1,24 @@
 package com.github.gbaso.cacc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CaCrossCompileApplication {
 
+  private static final Logger log = LoggerFactory.getLogger(CaCrossCompileApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(CaCrossCompileApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner hello() {
+    return args -> log.info("Hello World!");
   }
 
 }
